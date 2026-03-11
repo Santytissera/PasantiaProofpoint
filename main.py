7562d598f94006a63338cac3a07851a033b2e5b2
Santytissera <110065868+Santytissera@users.noreply.github.com>
2026-03-11 16:39:35 -0300

    añadidas filas en series csv

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import pandas as pd
+import datetime
 
-#Cargar el archivo csv
-fileInput = "Series.csv"
-outputFile = "episodes_clean.csv"
+fileinput = "Series.csv"
+fileoutput = "Episodes_clean.csv"
 
-df = pd.read_csv(fileInput,delimiter=',')
+df = pd.read_csv(fileinput)
 
-for i, row in df.iterrows():
-    if type(row["SeriesName"]) != str or row["SeriesName"] is None:
+valid_rows = []
+
+df["SeasonNumber"] = (pd.to_numeric(df["SeasonNumber"],"coerce").fillna(0).clip(lower=0).astype(int))
+df["EpisodeNumber"] = (pd.to_numeric(df["EpisodeNumber"],"coerce").fillna(0).clip(lower=0).astype(int))
+df["AirDate"] = pd.to_datetime(df["AirDate"],format="%Y-%m-%d",errors="coerce")
+df["AirDate"] = df["AirDate"].dt.strftime("%Y-%m-%d")
+
+lastRow = None
+for i,row in df.iterrows():
+
+    #Validacion de nombres de series
+    if not isinstance(row["SeriesName"],str):
         continue
     else:
-        print(row["SeriesName"].strip().lower().replace(" ",""))
+        row["SeriesName"] = (row["SeriesName"].strip()
+                             .lower()
+                             .replace(" ","")
+                             .encode("ascii","ignore")
+                             .decode("utf-8"))
 
+    if not isinstance(row["EpisodeTitle"],str):
+        row["EpisodeTitle"] = "Untitled Episode"
+    else:
+        row["EpisodeTitle"] = (row["EpisodeTitle"].strip()
+                             .lower()
+                             .replace(" ", "")
+                             .encode("ascii", "ignore")
+                             .decode("utf-8"))
+    if not isinstance(row["AirDate"],str):
+        row["AirDate"]  = "Unknown"
 
+    if lastRow is not None:
+        if row["SeriesName"] == lastRow["SeriesName"]:
 
-#Mostrar las primeras lineas del archivo
-#print(df.head())
+            print("Fila Duplicada")
 
+    lastRow = row
 
+#    valid_rows.append(row)
+#df_valid = pd.DataFrame(valid_rows)
+#df_valid.to_csv(fileoutput,index=False)
+
+#print(valid_rows)
